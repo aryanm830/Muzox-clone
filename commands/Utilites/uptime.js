70ef554a1cc842b65ce2a39bde7558c9cfe4da5e
Aryan <87185902+ARYANOP-Radio@users.noreply.github.com>
2022-10-03 21:24:12 +0530

    Update uptime.js

--- commands/Utilites/uptime.js
+++ commands/Utilites/uptime.js
@@ -11,7 +11,7 @@ const d = Math.round((Date.now() - client.uptime)/1000);
       const ms = require("ms");
       const em = new EmbedBuilder()
       .setColor(client.config.embedColor)
-.setAuthor(`Last Rebooted`, client.user.displayAvatarURL())
+.setAuthor({name:`Last Rebooted`,iconURL: client.user.displayAvatarURL()})
       .setDescription(`Uptime: ${ms(client.uptime)}\nLast Restarted: <t:${d}:R>`)
       message.channel.send({embeds: [em]})
   }
