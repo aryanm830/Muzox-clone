4eb329b7c2c26720b5967b3d618274b8a571213b
Aryan <87185902+ARYANOP-Radio@users.noreply.github.com>
2022-10-03 22:39:35 +0530

    Update nowplaying.js

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder, AttachmentBuilder } = require("discord.js");
-let canvafy = require ("canvafy");
+
 
  module.exports = {
     name : "nowplaying",
@@ -10,31 +10,12 @@ let canvafy = require ("canvafy");
     sameVc: true,
     player: true,
     run : async (client,message,args)=> {
-let player =  client.poru.players.get(message.guildId)
-    const spotify = await new canvafy.Spotify()
-    .setAuthor(player.currentTrack.info.author)
-    .setAlbum("World")
-    .setBackground("image", "https://cdn.discordapp.com/attachments/968080152640757762/1000017925362696282/img.jpg")
-    .setImage(player.currentTrack.info.image)
-.setTimestamp(player.position,  player.currentTrack.info.length)
-      .setTitle(player.currentTrack.info.title)
-    .build();
-      const attachment = new AttachmentBuilder(spotify.toBuffer(), 'nowplaying.png')
-      const embed = {
-        title:"Now Playing",
-        color: client.config.embedColor,
-        image: {
-          url:`attachment://nowplaying.png`
-        }
-      };
-      
-    message.channel.send({
-      embeds: [embed],
-   files: [{
-        attachment: spotify.toBuffer(),
-        name: `nowplaying.png`
-      }]});
-
+let player =  client.poru.players.get(message.guildId);
+   const em = new EmbedBuilder()
+   .setImage(player.currentTrack.info.image)
+   .setDescription(`Title: ${player.currentTrack.info.title}`)
+   message.channel.send({embeds:[em]})
+   
 
       
 }}
